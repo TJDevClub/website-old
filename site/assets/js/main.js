663d8c9b7b464afc6117c39b9f34f823233951c9
Alex-Gurung <aag1234@gmail.com>
2017-02-05 17:07:05 -0500

    formatting and js scoping

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,43 +1,41 @@
 /* globals Typer */
 /* jshint latedef: nofunc*/
-
 'use strict';
-window.onload = function () {
-	
-	var title = $('title').text();
-	if (title.includes('Archives')) {
-		
-		$("#reposis").loadRepositories("TJDevClub", -1);
-	}
-	else{
-		var typer = new Typer(document.querySelector('.computer'));
-	typer.addText('dev -hi');
-	typer.newLine();
-	typer.addText('Welcome to Dev Club...');
-	typer.newLine();
-	typer.addText('We make cool stuff.');
-	typer.newLine();
-	typer.addHtml('<a class="button join-us" href="./#join"> Join Us </a>');
-	typer.type();
+window.onload = function() {
 
-	document.querySelector('.burger-container').addEventListener('click', function () {
-		console.log('hi');
-		this.querySelector('.burger').classList.toggle('x');
+  var title = $('title').text();
+  if (title.includes('Archives')) {
 
-		document.querySelector('nav').classList.toggle('visible');
-	});
+    $("#reposis").loadRepositories("TJDevClub", -1);
+  } else {
+    var typer = new Typer(document.querySelector('.computer'));
+    typer.addText('dev -hi');
+    typer.newLine();
+    typer.addText('Welcome to Dev Club...');
+    typer.newLine();
+    typer.addText('We make cool stuff.');
+    typer.newLine();
+    typer.addHtml('<a class="button join-us" href="./#join"> Join Us </a>');
+    typer.type();
 
-	var navButtons = document.querySelectorAll('nav > ul > li > a');
-	for (var i = navButtons.length - 1; i >= 0; i--) {
-		navButtons.item(i).addEventListener('click', closeNav);
-	}
-		$("#reposis").loadRepositories("TJDevClub", 4);
-	}
-	//$("#reposis").loadRepositories("TJDevClub", 4);
-	
+    document.querySelector('.burger-container').addEventListener('click', function() {
+      console.log('hi');
+      this.querySelector('.burger').classList.toggle('x');
+
+      document.querySelector('nav').classList.toggle('visible');
+    });
+
+    var navButtons = document.querySelectorAll('nav > ul > li > a');
+    for (var i = navButtons.length - 1; i >= 0; i--) {
+      navButtons.item(i).addEventListener('click', closeNav);
+    }
+    $("#reposis").loadRepositories("TJDevClub", 4);
+  }
+  //$("#reposis").loadRepositories("TJDevClub", 4);
+
 };
 
-function closeNav () {
-	document.querySelector('nav').classList.remove('visible');
-	document.querySelector('.burger').classList.remove('x');
+function closeNav() {
+  document.querySelector('nav').classList.remove('visible');
+  document.querySelector('.burger').classList.remove('x');
 }
--- site/assets/js/repositories.js
+++ site/assets/js/repositories.js
@@ -1,49 +1,46 @@
 jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
+  jQuery.getJSON('https://api.github.com/users/' + username + '/repos?callback=?', callback)
 }
- 
+
 jQuery.fn.loadRepositories = function(username, maxCount) {
-    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>"); //Should only exist while loading
-     
-    var target = this;
-    $.githubUser(username, function(data) {
-        var repos = data.data; // JSON Parsing
-		//console.log(repos)
+  this.html("<span>Querying GitHub for " + username + "'s repositories...</span>"); //Should only exist while loading
 
-        sortByName(repos); //Puts them in date-order
-     
-        var list = $('<div/>');
-		var counter = 0;
-        target.empty().append(list);
-        $(repos).each(function() {
-			let description = this.description;
-			if (!description){
-				description = " ";
-			}
-			//console.log(encodeURI(description));
-			var utDescription = encodeURI(description);
-			//console.log(encodeURI('🍠'));
-			if (!utDescription.includes("%F0%9F%8D%A0")){ //Not presentations
-			counter+=1;
-			if (maxCount == -1 || counter <=maxCount){ //Appending four lectures
-            if (this.name != (username.toLowerCase()+'.github.com')) {
-                var date = this.updated_at.split('-');
-				var year = date[0];
-				var month = date[1];
-				var theRest = date[2].split('T');
-				var day = theRest[0];
-				var str = '<li class="lecture-item">' + '<a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + '<div class="lecture-title">'+month+'-' + day+'-' + year + ': ' + this.name + '</div>' + '<div class="lecture-presenter">' + description +'</div>'+ '</a>'
-				$('#reposis').append(str); //Append html and data
-            }}
-			}
-        });      
-      });
-      
-    function sortByName(repos) {
-		//console.log(repos);
-        repos.sort(function(a,b) {
-			return b.updated_at.localeCompare(a.updated_at); //Compare when last updated (first is most recent)
-       });
-    }
+  var target = this;
+  $.githubUser(username, function(data) {
+    let repos = data.data; // JSON Parsing
+    //console.log(repos)
+
+    sortByName(repos); //Puts them in date-order
+
+    let list = $('<div/>');
+    let counter = 0;
+    target.empty().append(list);
+    $(repos).each(function() {
+      let description = this.description;
+      if (!description) {
+        description = " ";
+      }
+      const utDescription = encodeURI(description);
+      if (!utDescription.includes("%F0%9F%8D%A0")) { //Not presentations
+        counter += 1;
+        if (maxCount == -1 || counter <= maxCount) { //Appending four lectures
+          if (this.name != (username.toLowerCase() + '.github.com')) {
+            const date = this.updated_at.split('-');
+            const year = date[0];
+            const month = date[1];
+            const theRest = date[2].split('T');
+            const day = theRest[0];
+            const str = '<li class="lecture-item">' + '<a href="' + (this.homepage ? this.homepage : this.html_url) + '">' + '<div class="lecture-title">' + month + '-' + day + '-' + year + ': ' + this.name + '</div>' + '<div class="lecture-presenter">' + description + '</div>' + '</a>'
+            $('#reposis').append(str); //Append html and data
+          }
+        }
+      }
+    });
+  });
+
+  function sortByName(repos) {
+    repos.sort(function(a, b) {
+      return b.updated_at.localeCompare(a.updated_at); //Compare when last updated (first is most recent)
+    });
+  }
 };
-
